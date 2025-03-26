ff2e7e44cf463e0ec5061e8fca62aae616a24c53
Miguel231 <miguelpitufet@gmail.com>
2025-03-26 20:55:14 +0100

    okay

---
+++ AAgent_Python/Goals_BT.py
@@ -0,0 +1,61 @@
+import math
+import random
+import asyncio
+import Sensors
+from collections import Counter
+
+
+class DoNothing:
+    """
+    Does nothing
+    """
+    def __init__(self, a_agent):
+        self.a_agent = a_agent
+        self.rc_sensor = a_agent.rc_sensor
+        self.i_state = a_agent.i_state
+
+    async def run(self):
+        print("Doing nothing")
+        await asyncio.sleep(1)
+        return True
+
+class ForwardStop:
+    """
+        Moves forward till it finds an obstacle. Then stops.
+    """
+    STOPPED = 0
+    MOVING = 1
+    END = 2
+
+    def __init__(self, a_agent):
+        self.a_agent = a_agent
+        self.rc_sensor = a_agent.rc_sensor
+        self.i_state = a_agent.i_state
+        self.state = self.STOPPED
+
+    async def run(self):
+        try:
+            while True:
+                if self.state == self.STOPPED:
+                    # Start moving
+                    await self.a_agent.send_message("action", "mf")
+                    self.state = self.MOVING
+                elif self.state == self.MOVING:
+                    sensor_hits = self.rc_sensor.sensor_rays[Sensors.RayCastSensor.HIT]
+                    if any(ray_hit == 1 for ray_hit in sensor_hits):
+                        self.state = self.END
+                        await self.a_agent.send_message("action", "stop")
+                    else:
+                        await asyncio.sleep(0)
+                elif self.state == self.END:
+                    break
+                else:
+                    print("Unknown state: " + str(self.state))
+                    return False
+        except asyncio.CancelledError:
+            print("***** TASK Forward CANCELLED")
+            await self.a_agent.send_message("action", "stop")
+            self.state = self.STOPPED
+
+
+
